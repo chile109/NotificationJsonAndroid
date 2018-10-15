9547d0e5ab267362e531bc619155cb0e59c75b58
Kevin Chang <kevin@megalive.com.tw>
2018-10-15 17:23:36 +0800

    [Add] 獨立Message class

---
+++ app/src/main/java/app/kevin/com/jsontonotification/Message.java
@@ -0,0 +1,27 @@
+package app.kevin.com.jsontonotification;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Message {
+        public String title;
+        public String content;
+        public Date date;
+
+        public Message(String title, String contenxt, Date date) {
+            this.title = title;
+            this.content = contenxt;
+            this.date = date;
+        }
+
+        @Override
+        public String toString() {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS", Locale.CHINESE);
+            return "Data{" +
+                    "title=" + title +
+                    ", content=" + content +
+                    ", date=" + simpleDateFormat.format(date) +
+                    '}';
+        }
+}
