4804fd9b8dda326e00dc1ebe60a38d09165d0145
Kevin Chang <kevin@megalive.com.tw>
2018-10-15 12:00:24 +0800

    [Update] 以AlertManager觸發Notification

--- app/src/main/java/app/kevin/com/jsontonotification/MainActivity.java
+++ app/src/main/java/app/kevin/com/jsontonotification/MainActivity.java
@@ -1,5 +1,6 @@
 package app.kevin.com.jsontonotification;
 
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -10,78 +11,119 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-	public static final String idLove = "default";
-	private String Testdata = "{\"title\":\"晚餐服藥\",\"content\":\"紅包配溫開水\", \"date\":\"2018-03-17 19:55:30:033\"}";
+	private AlarmManager alarmManager;
+	private PendingIntent pendingIntent;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		NotificationChannel channelLove = new NotificationChannel(
-				idLove,
-				"Channel Love",
-				NotificationManager.IMPORTANCE_HIGH);
-		channelLove.setDescription("最重要的人");
-		channelLove.enableLights(true);
-		channelLove.enableVibration(true);
-
-		NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-		notificationManager.createNotificationChannel(channelLove);
-
-		Notification.Builder builder =
-				new Notification.Builder(this)
-						.setSmallIcon(R.drawable.ic_launcher_background)
-						.setContentTitle(JsonToObject(Testdata).title)
-						.setContentText(JsonToObject(Testdata).content)
-						.setChannelId(idLove);
-		notificationManager.notify(1, builder.build());
-
-
+		initView();
+		initAlarm();
 	}
 
-	private Message JsonToObject(String data) {
-		Gson gson = new GsonBuilder()
-				.setPrettyPrinting()//格式化输出
-				.setDateFormat("yyyy-MM-dd HH:mm:ss:SSS")//格式化时间
-				.create();
+	// 初始化控件
+	private void initView() {
 
-		Message m = gson.fromJson(data, Message.class);
-		System.out.println();
-		System.out.println(m);
+		Button alarmBtn = (Button) findViewById(R.id.button);
+		alarmBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// 设置闹钟时间
+				Calendar calendar = Calendar.getInstance();
+				calendar.setTimeInMillis(System.currentTimeMillis());
+				calendar.set(Calendar.HOUR_OF_DAY, 11);
+				calendar.set(Calendar.MINUTE, 56);
+				calendar.set(Calendar.SECOND, 30);
 
-		return m;
+				setAlarm(calendar);
+			}
+		});
 	}
 
-	class Message {
-		private String title;
-		private String content;
-		private Date date;
+	// 初始化闹钟
+	private void initAlarm() {
+		// 实例化AlarmManager
+		alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+		// 设置闹钟触发动作
+		Intent intent = new Intent(this, AlarmBroadcast.class);
+		intent.setAction("startAlarm");
+		pendingIntent = PendingIntent.getBroadcast(this, 110, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+		// 设置闹钟
+		/**
+		 * AndroidL开始，设置的alarm的触发时间必须大于当前时间 5秒
+		 *
+		 * 设置一次闹钟-(5s后启动闹钟)
+		 *
+		 * @AlarmType int type：闹钟类型
+		 * ELAPSED_REALTIME：在指定的延时过后，发送广播，但不唤醒设备（闹钟在睡眠状态下不可用）。如果在系统休眠时闹钟触发，它将不会被传递，直到下一次设备唤醒。
+		 * ELAPSED_REALTIME_WAKEUP：在指定的延时过后，发送广播，并唤醒设备（即使关机也会执行operation所对应的组件）。延时是要把系统启动的时间SystemClock.elapsedRealtime()算进去的。
+		 * RTC：指定当系统调用System.currentTimeMillis()方法返回的值与triggerAtTime相等时启动operation所对应的设备（在指定的时刻，发送广播，但不唤醒设备）。如果在系统休眠时闹钟触发，它将不会被传递，直到下一次设备唤醒（闹钟在睡眠状态下不可用）。
+		 * RTC_WAKEUP：指定当系统调用System.currentTimeMillis()方法返回的值与triggerAtTime相等时启动operation所对应的设备（在指定的时刻，发送广播，并唤醒设备）。即使系统关机也会执行 operation所对应的组件。
+		 *
+		 * long triggerAtMillis：触发闹钟的时间。
+		 * PendingIntent operation：闹钟响应动作（发广播，启动服务等）
+		 */
+//        alarmManager.set(@AlarmType int type, long triggerAtMillis, PendingIntent operation);
 
-		public Message(String title, String contenxt, Date date) {
-			this.title = title;
-			this.content = contenxt;
-			this.date = date;
-		}
+		/**
+		 * AndroidL开始repeat的周期必须大于60秒
+		 *
+		 * 设置精准周期闹钟-该方法提供了设置周期闹钟的入口，闹钟执行时间严格按照startTime来处理，使用该方法需要的资源更多，不建议使用。
+		 *
+		 * @AlarmType int type：闹钟类型
+		 * long triggerAtMillis：触发闹钟的时间。
+		 * long intervalMillis：闹钟两次执行的间隔时间
+		 * PendingIntent operation：闹钟响应动作（发广播，启动服务等）
+		 */
+//        alarmManager.setRepeating(@AlarmType int type, long triggerAtMillis, long intervalMillis, PendingIntent operation);
 
-		@Override
-		public String toString() {
-			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS", Locale.CHINESE);
-			return "Data{" +
-					"title=" + title +
-					", content=" + content +
-					", date=" + simpleDateFormat.format(date) +
-					'}';
-		}
+		/**
+		 * AndroidL开始repeat的周期必须大于60秒
+		 *
+		 * 设置不精准周期闹钟- 该方法也用于设置重复闹钟，与第二个方法相似，不过其两个闹钟执行的间隔时间不是固定的而已。它相对而言更省电（power-efficient）一些，因为系统可能会将几个差不多的闹钟合并为一个来执行，减少设备的唤醒次数。
+		 *
+		 * @AlarmType int type：闹钟类型
+		 * long triggerAtMillis：触发闹钟的时间。
+		 * long intervalMillis：闹钟两次执行的间隔时间
+		 * 内置变量
+		 * INTERVAL_DAY： 设置闹钟，间隔一天
+		 * INTERVAL_HALF_DAY： 设置闹钟，间隔半天
+		 * INTERVAL_FIFTEEN_MINUTES：设置闹钟，间隔15分钟
+		 * INTERVAL_HALF_HOUR： 设置闹钟，间隔半个小时
+		 * INTERVAL_HOUR： 设置闹钟，间隔一个小时
+		 *
+		 * PendingIntent operation：闹钟响应动作（发广播，启动服务等）
+		 */
+//        alarmManager.setInexactRepeating(@AlarmType int type, long triggerAtMillis, long intervalMillis, PendingIntent operation);
+
+		// 取消闹钟
+//        alarmManager.cancel(pendingIntent);
+	}
+
+	// 设置闹钟
+	private void setAlarm(Calendar calendar) {
+//        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (1000 * 5), pendingIntent);
+		alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+
+		Toast.makeText(this, "设置成功", Toast.LENGTH_SHORT).show();
 
 	}
 }
