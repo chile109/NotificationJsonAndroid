ee6a4c8504a77450cd28022d2d439801ee94d36c
Kevin Chang <kevin@megalive.com.tw>
2018-10-16 15:53:05 +0800

    [Update] Gson deserialized array object

--- app/src/main/java/app/kevin/com/jsontonotification/MainActivity.java
+++ app/src/main/java/app/kevin/com/jsontonotification/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,12 +20,14 @@ import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import junit.framework.Test;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,11 +35,20 @@ public class MainActivity extends AppCompatActivity {
 	private PendingIntent pendingIntent;
 	public static Message _message;
 	private String Testdata = "{\"title\":\"晚餐服藥\",\"content\":\"紅包配溫開水\", \"date\":\"2018-10-15 18:00:10:033\"}";
-
+	private String TestData2 = "[{\"title\":\"中午注射\",\"content\":\"胰島素30cc\",\"date\":\"2018-10-15 18:00:10:033\"},{\"title\":\"晚間服藥\",\"content\":\"紅包配白開水\",\"date\":\"2018-10-15 18:00:10:033\"}]";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Gson gson = new GsonBuilder()
+				.setPrettyPrinting()//格式化输出
+				.setDateFormat("yyyy-MM-dd HH:mm:ss:SSS")//格式化时间
+				.create();
+
+		Message[] messageArray = gson.fromJson(TestData2, Message[].class);
+		System.out.println(messageArray[0]);
+		
 		_message = JsonToMessage(Testdata);
 		initView();
 		initAlarm();
@@ -78,7 +90,6 @@ public class MainActivity extends AppCompatActivity {
 
 	// 设置闹钟
 	private void setAlarm(Calendar calendar) {
-//        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (1000 * 5), pendingIntent);
 		alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
 
 		Toast.makeText(this, "设置成功", Toast.LENGTH_SHORT).show();
